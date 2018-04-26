96e218748a45bd2ee29150dab0b1c94df8020489
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-26 16:23:40 +0800

    Initial Preparation for backup

--- class/MySqlLeaf.php
+++ class/MySqlLeaf.php
@@ -2,16 +2,21 @@
 
 class MySqlLeaf {
 	private static $dbCon = NULL;
+	private static $dbCon2 = NULL;
+	private static $host = "localhost";
+	private static $user = "root";
+	private static $pass = "";
+	private static $name = "aims";
 
 	public static function getCon(){
 		if (self::$dbCon === NULL){
 
 			try {
 				$db = mysqli_connect(
-					"localhost",
-					"root",
-					"",
-					"aims"
+					self::$host,
+					self::$user,
+					self::$pass,
+					self::$name
 				);
 			} catch(Exception $e) {
 				echo $e->getMessage();
@@ -22,6 +27,18 @@ class MySqlLeaf {
 		}
 
 		return self::$dbCon;
+	}
+
+	public static function getCon2(){
+		if (self::$dbCon2 === NULL){
+			try {
+				$db = new mysqli(self::$host,self::$user,self::$pass,self::$name);;
+			} catch(Exception $e) {
+				echo $e->getMessage();
+				$db = NULL;
+			}
+			self::$dbCon2 = $db;
+		}
 	}
 
 }
